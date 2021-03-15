9543e70962e7978d5e92b5766795e855deae3477
Lilia García Hiramatsu <liliagarciahiramatsu@gmail.com>
2021-03-15 14:36:26 -0300

    Delete TP_3 directory

--- TP_3/crear.h
+++
@@ -1,6 +0,0 @@
-#ifndef CREAR_H
-#define CREAR_H
-#include <stdio.h>
-#include <stdlib.h>
-double *crear_vector(int N);
-#endif
--- TP_3/crear_vector.c
+++
@@ -1,6 +0,0 @@
-#include "crear.h"
-double *crear_vector(int N) {
-    double *v;
-    v = (double *)malloc(N*sizeof(double));
-    return v;
-}
